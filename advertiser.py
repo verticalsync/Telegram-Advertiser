9532a20cfb88504110c69152cfc02a33613af909
purpl3r0se <purpl3r0se@protonmail.com>
2022-07-14 19:45:55 +0300

    updated frfr

--- advertiser.py
+++ advertiser.py
@@ -3,14 +3,13 @@ from telethon import TelegramClient, errors
 from telethon.tl.functions.channels import GetFullChannelRequest
 from colorama import Fore
 
-clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-
-clear()
 os.system("title Telegram Advertiser ^| purpl3r0se#0001")
+clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+clear()
 
 with open("config.txt", "a+") as config:
     config.seek(0)
-    cfg = config.read().strip()
+    cfg = config.read().strip().split("\n")[0]
     if cfg == "api_id:api_hash":
         print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Please edit config.txt with your api id and api hash")
         time.sleep(9999)
@@ -27,21 +26,67 @@ with open("config.txt", "a+") as config:
             time.sleep(9999)
             exit()
 
-client = TelegramClient('anon', api_id, api_hash)
+try: os.mkdir("session")
+except: pass
+
+client = TelegramClient('session/session', api_id, api_hash)
 
 sent = 0
 groups = open("groups.txt", "r+").read().strip().split("\n")
-message = open("message.txt", "r+", encoding="utf8").read().strip()
+forwardgroup = int(open("forward.txt", "r+", encoding="utf8").read().strip().split("\n")[0])
 found_groups = []
 
 wait1 = int(input(f"{Fore.WHITE}[{Fore.MAGENTA}?{Fore.WHITE}]{Fore.RESET} How long do you want to wait between each message? (seconds): "))
 wait2 = int(input(f"{Fore.WHITE}[{Fore.MAGENTA}?{Fore.WHITE}]{Fore.RESET} How long do you want to wait after all groups have been messaged? (seconds): "))
 print("")
 
+lefttowait = 0
+
 def title():
-    os.system(f"title Telegram Advertiser ^| purpl3r0se#0001 ^| Groups: {len(found_groups)}, Messages Sent: {sent}")
+    text = f"Telegram Advertiser ^| purpl3r0se#0001 ^| Groups: {len(found_groups)}, Messages Sent: {sent}"
+    text += f" ^| Sleeping for {lefttowait} seconds" if lefttowait > 0 else ""
+    os.system(f"title {text}")
+
+def sleep(towait):
+    global lefttowait
+    for i in range(towait):
+        lefttowait = towait - i
+        title()
+        time.sleep(1)
+    lefttowait = 0
+    title()
+
+async def send(forwarded_message):
+    global sent
+
+    while True:
+        for found_group in found_groups:
+            group = found_group
+            try:
+                await client.forward_messages(group, forwarded_message)
+                print(f"{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}]{Fore.RESET} Message sent to {group}, Sleeping for {wait1} seconds")
+                sent += 1
+                title()
+                sleep(wait1)
+            except errors.rpcerrorlist.SlowModeWaitError:
+                print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Failed to send to channel {group}, due to slowmode.")
+                title()
+                sleep(wait1)
+            except errors.rpcerrorlist.ChannelPrivateError:
+                print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Failed to send to channel {group}, due to it being private or you've been banned, removing it from list.")
+                found_groups.remove(found_group)
+                title()
+                sleep(wait1)
+            except Exception as e:
+                print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Failed to send to channel {group}, due to it being an unhandled exception here's the direct reason: {e}, {e.args}")
+                title()
+                sleep(wait1)
+        print(f"{Fore.WHITE}[{Fore.YELLOW}\{Fore.WHITE}]{Fore.RESET} Sleeping for {wait2} seconds, because all groups have been sent to.")
+        sleep(wait2)
 
 async def x():
+    forwarded_message = (await client.get_messages(forwardgroup, limit=50))[0]
+
     async for dialog in client.iter_dialogs():
         if dialog.is_group:
             with contextlib.suppress(Exception):
@@ -50,41 +95,16 @@ async def x():
                         for chat in inv.chats:
                             if chat.username.lower() == group.lower():
                                 found_groups.append(f"{chat.username}")
+                                title()
 
     v = [found_group.lower() for found_group in found_groups]
     for group in groups:
         if group.lower() not in v:
             print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Group {group} not found")
+            time.sleep(1)
 
-    time.sleep(3)
     os.system("cls")
-    global sent
-
-    while True:
-        for found_group in found_groups:
-            group = found_group
-            try:
-                await client.send_message(group, message)
-                print(f"{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}]{Fore.RESET} Message sent to {group}, Sleeping for {wait1} seconds")
-                sent += 1
-                title()
-                time.sleep(wait1)
-            except errors.rpcerrorlist.SlowModeWaitError:
-                print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Failed to send to channel {group}, due to slowmode.")
-                title()
-                time.sleep(wait1)
-            except errors.rpcerrorlist.ChannelPrivateError:
-                print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Failed to send to channel {group}, due to it being private or you've been banned, removing it from list.")
-                found_groups.remove(found_group)
-                title()
-                time.sleep(wait1)
-            except Exception as e:
-                print(f"{Fore.WHITE}[{Fore.RED}!{Fore.WHITE}]{Fore.RESET} Failed to send to channel {group}, due to it being an unhandled exception here's the direct reason: {e}, {e.args}")
-                title()
-                time.sleep(wait1)
-        print(f"{Fore.WHITE}[{Fore.YELLOW}\{Fore.WHITE}]{Fore.RESET} Sleeping for {wait2} seconds, because all groups have been sent to.")
-        time.sleep(wait2)
-
+    await send(forwarded_message)
 
 client.start()
 client.loop.run_until_complete(x())
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+python advertiser.py
+echo.
+echo closed
+pause >NUL
